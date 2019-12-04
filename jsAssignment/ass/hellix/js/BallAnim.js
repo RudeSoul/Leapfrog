398754f741cb520b9c509004a3aec3607ed5375e
Prabesh <prabesh7@gmail.com>
2019-12-05 00:39:57 +0545

    helix animaion added

---
+++ jsAssignment/ass/hellix/js/BallAnim.js
@@ -0,0 +1,39 @@
+class BallAnim{
+    constructor(context, ballCount, radius){
+      this.context = context;
+      this.ballCount = ballCount;    
+      this.maxCircleRadius = radius; 
+      this.ballVGap = 15;
+      this.ballHGap = 18;
+      this.amplitude = 40;
+      this.frequency = 2
+      this.topPad = 50;
+      this.leftPad = 50;
+    }
+
+    gradColor = function (){
+        return `red` ;
+    }
+
+    update = (colInd, vShift) =>{
+        var xPos = colInd * this.ballHGap + this.leftPad;
+        var colIndShift = this.frequency * (colInd * Math.PI) / this.amplitude;    
+        for(var ballInd = 0; ballInd < this.ballCount; ballInd++){ 
+            var yPos =  this.topPad + ballInd * this.ballVGap +  Math.sin(vShift + colIndShift) * this.amplitude;
+            var changeSize = (Math.cos(vShift - (ballInd * 0.1) + colIndShift) + 1)/2;        
+            var radius = changeSize * this.maxCircleRadius;
+            
+            this.draw(xPos, yPos, radius, this.gradColor());
+        }
+    }
+    
+    draw = (xPos, yPos, radius, color) =>{     
+        this.context.beginPath();
+        this.context.arc(xPos, yPos, radius, 0, Math.PI * 2, false);
+        this.context.fillStyle = color;
+        this.context.fill();
+        this.context.closePath();
+      }      
+  
+  }
+  
